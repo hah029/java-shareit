f3340e72c194cabfb07d82c6f74efe9cb4aa4a28
Roman <roma.chekalin@gmail.com>
2025-11-05 16:51:09 +0300

    Sprint 16: fix for style checking

--- src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
+++ src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -1,12 +1,10 @@
 package ru.practicum.shareit.request.dto;
 
 import lombok.Data;
-import ru.practicum.shareit.item.dto.CommentDto;
 import ru.practicum.shareit.item.dto.ItemDto;
 
 import java.time.LocalDateTime;
 import java.util.List;
-
 
 @Data
 public class ItemRequestDto {
