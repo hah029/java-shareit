b43820fc9a5f519e6f280f35c2e32ebe96e998a9
Roman <roma.chekalin@gmail.com>
2025-11-08 04:07:27 +0300

    Sprint 16: fix for review #2

--- gateway/src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -22,12 +22,12 @@ import static ru.practicum.shareit.Constant.OWNER_HEADER;
 @Slf4j
 @Validated
 public class BookingController {
-	private final BookingClient client;
+    private final BookingClient client;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<Object> create(@RequestHeader(OWNER_HEADER) @Positive @NotNull long userId,
-                                         @RequestBody @Valid BookingCreateRequestDto requestDto) {
+            @RequestBody @Valid BookingCreateRequestDto requestDto) {
         log.info("Creating booking {}, userId={}", requestDto, userId);
         return client.create(userId, requestDto);
     }
@@ -35,8 +35,8 @@ public class BookingController {
     @PatchMapping("/{bookingId}")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<Object> approve(@RequestHeader(OWNER_HEADER) @Positive @NotNull long userId,
-                                          @PathVariable @Positive @NotNull long bookingId,
-                                          @RequestParam boolean approved) {
+            @PathVariable @Positive @NotNull long bookingId,
+            @RequestParam boolean approved) {
         log.info("Получен запрос на подтверждение бронирования с id={} от пользователя с id={}", bookingId, userId);
         return client.approve(userId, bookingId, approved);
     }
@@ -44,34 +44,33 @@ public class BookingController {
     @GetMapping("/{bookingId}")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<Object> get(@RequestHeader(OWNER_HEADER) @Positive @NotNull long userId,
-                                      @PathVariable @Positive @NotNull long bookingId) {
+            @PathVariable @Positive @NotNull long bookingId) {
         log.info("Get booking {}, userId={}", bookingId, userId);
         return client.get(userId, bookingId);
     }
 
-	@GetMapping
+    @GetMapping
     @ResponseStatus(HttpStatus.OK)
-	public ResponseEntity<Object> getAllByUser(@RequestHeader(OWNER_HEADER) long userId,
-			@RequestParam(name = "state", defaultValue = "all") String stateParam,
-			@PositiveOrZero @RequestParam(name = "from", defaultValue = "0") Integer from,
-			@Positive @RequestParam(name = "size", defaultValue = "10") Integer size) {
-		BookingState state = BookingState.from(stateParam)
-				.orElseThrow(() -> new IllegalArgumentException("Unknown state: " + stateParam));
-		log.info("Get booking with state {}, userId={}, from={}, size={}", stateParam, userId, from, size);
-		return client.getAllByUser(userId, state, from, size);
-	}
+    public ResponseEntity<Object> getAllByUser(@RequestHeader(OWNER_HEADER) long userId,
+            @RequestParam(name = "state", defaultValue = "all") String stateParam,
+            @PositiveOrZero @RequestParam(name = "from", defaultValue = "0") Integer from,
+            @Positive @RequestParam(name = "size", defaultValue = "10") Integer size) {
+        BookingState state = BookingState.from(stateParam)
+                .orElseThrow(() -> new IllegalArgumentException("Unknown state: " + stateParam));
+        log.info("Get booking with state {}, userId={}, from={}, size={}", stateParam, userId, from, size);
+        return client.getAllByUser(userId, state, from, size);
+    }
 
     @GetMapping("/owner")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<Object> getAllByOwner(@RequestHeader(OWNER_HEADER) Long userId,
-                                          @RequestParam(defaultValue = "ALL") String stateParam,
-                                          @RequestParam(defaultValue = "0") Integer from,
-                                          @RequestParam(defaultValue = "10") Integer size) {
+            @RequestParam(defaultValue = "ALL") String stateParam,
+            @RequestParam(defaultValue = "0") Integer from,
+            @RequestParam(defaultValue = "10") Integer size) {
         BookingState state = BookingState.from(stateParam)
                 .orElseThrow(() -> new IllegalArgumentException("Unknown state: " + stateParam));
         log.info("Получен запрос на получение всех бронирований владельца с id={}, state={}, from={}, size={}", userId,
                 state, from, size);
         return client.getAllByOwner(userId, state, from, size);
     }
-
 }
